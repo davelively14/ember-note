fda34aeaa92d48e4bcbeb7a529510390e7af61c6
Dave Lively <davelively14@gmail.com>
2015-12-26 07:51:10 -0500

    Edited login/route.js for readability

--- app/login/route.js
+++ app/login/route.js
@@ -7,12 +7,12 @@ export default Ember.Route.extend({
       this.store.query('user', {
         name: this.controller.get('name')
       }).then((users) => {
+
         if(users.get('length') === 1) {
           var user = users.objectAt(0);
           this.controllerFor('application').set('user', user);
           this.transitionTo('notebooks', user.get('id'));
-        }
-        else {
+        } else {
           console.log('unexpected query result');
         }
       });
